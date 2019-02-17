c08dc7464b16a352e0f1625d5a1231c2ae82db5e
Kened Wanderson <kenedwanderson@live.com>
2019-02-17 16:39:10 -0300

    start

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//  begin
+//     while (not last character) do
+//       begin
+//         readSymbol()
+//         shorten context
+//         while (context not found and context length not -1) do
+//          begin
+//            output(escape sequence)
+//            shorten context
+//          end
+//         output(character)
+//         while (context length not -1) do
+//          begin
+//            increase count of character (create node if nonexistant)
+//            shorten context
+//          end
+//       end
+//   end  
